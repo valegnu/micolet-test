37a5f4cba522e45ea59523f358ffe54c74c3c93f
Valentin <94926836+valegnu@users.noreply.github.com>
2021-11-28 15:11:18 -0600

    Refactor JS Code

--- app/javascript/packs/landing.js
+++ app/javascript/packs/landing.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
     init()
-    
+
     const element = document.querySelector("#submit-btn")
 
     element.addEventListener("click", (event) => {
@@ -9,7 +9,12 @@ window.addEventListener("load", () => {
     });
 });
 
-// Vars
+function init() {
+    document.querySelector("#errors-container").style.display = 'none'
+    document.querySelector("#progress").style.display = 'none'
+}
+
+// Var functions
 function email() {
     return document.querySelector("#suscription_email")
 }
@@ -21,12 +26,8 @@ function checks() {
 function progress() {
     return document.querySelector("#progress")
 }
-// Functions
-function init() {
-    document.querySelector("#errors-container").style.display = 'none'
-    document.querySelector("#progress").style.display = 'none'
-}
 
+// Process Form
 function perform() {
     if (emailHasValue() && preferencesIsSelected())
 	sendRequest()
@@ -34,6 +35,7 @@ function perform() {
 	showIncompleteMessage()
 }
 
+// Validators functions
 function emailHasValue() {
     if (email().value.length > 0)
 	return true
@@ -50,6 +52,7 @@ function preferencesIsSelected() {
     return false
 }
 
+// Request functions
 function constructPreferences() {
     var preferences = '{'
 
@@ -62,44 +65,48 @@ function constructPreferences() {
     return JSON.parse(preferences.substring(0, preferences.length - 1) + '}')
 }
 
+function bodyRequest() {
+    return {
+	locale: document.querySelector("#locale").value,
+	email: email().value,
+	preferences: constructPreferences()
+    }
+}
+
 function sendRequest() {
     progress().style.display = 'block'
-        
+
     fetch('/new_suscription', {
 	method: 'POST',
-	headers: {
-	    'Content-Type': 'application/json',
-	},
-	body: JSON.stringify({
-	    locale: document.querySelector("#locale").value,
-	    email: email().value,
-	    preferences: constructPreferences()
-	}),
+	headers: { 'Content-Type': 'application/json' },
+	body: JSON.stringify(bodyRequest()),
     })
-    .then(response => response.json())
-    .then(data => {
-
-	if (data['message'] == "error")
-	    showErrors(data)
-	else
-	    sucess(data)
-	    
-    })
-    .catch((error) => {
-        progress().style.display = 'none'
-	showUnknownError()
-    });
+	.then(response => response.json())
+	.then(data => {
+            processResponse(data)
+	})
+	.catch((error) => {
+            processUnsuccessResponse()
+	});
 }
 
-function sucess(data) {
+// Manage responses
+function processResponse(data) {
     progress().style.display = 'none'
-    cleanForm()
-    showSuccessMessage()
+
+    if (data['message'] == "error")
+	showErrors(data)
+    else
+	success()
 }
 
+function processUnsuccessResponse() {
+    progress().style.display = 'none'
+    showUnknownError()
+}
+
+// Call to actions response case
 function showErrors(data) {
-    progress().style.display = 'none'
-    
     var html = "";
 
     data['errors'].forEach((element) => {
@@ -108,6 +115,11 @@ function showErrors(data) {
 
     document.querySelector("#errors-msg").innerHTML = html
     document.querySelector("#errors-container").style.display = 'block'
+}
+
+function success() {
+    cleanForm()
+    showSuccessMessage()
 }
 
 function cleanForm() {
@@ -122,6 +134,7 @@ function cleanChecks() {
     });
 }
 
+// Show messages functions
 function showUnknownError() {
     M.toast( { html: document.querySelector("#unknown-error").value } )
 }
